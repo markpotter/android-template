305a523da5b15e5bf533f770627ea25eb5b431f3
markpotter <mpotter@familysearch.org>
2022-11-21 13:55:01 -0700

    Inject MainViewModel into MainScreen composable

--- app/src/main/java/com/noumenon/template/MainActivity.kt
+++ app/src/main/java/com/noumenon/template/MainActivity.kt
@@ -8,10 +8,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.noumenon.template.ui.theme.AndroidTemplateTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
       AndroidTemplateTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          MainScreen("Android")
         }
       }
     }
@@ -27,14 +32,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun MainScreen(name: String) {
+  val viewModel: MainViewModel = hiltViewModel()
+  val state by viewModel.greeting.collectAsState()
+  Text(text = "$state $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
   AndroidTemplateTheme {
-    Greeting("Android")
+    MainScreen("Android")
   }
 }
